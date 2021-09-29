4bdc4c9f9771cad1b8aa104f4feb32c6cd4e2f25
tũ4an <foeveralone999@gmail.com>
2021-09-29 22:27:59 +0700

    baitap2

--- src/com/tuanpm167/lap1/baitap2.java
+++ src/com/tuanpm167/lap1/baitap2.java
@@ -4,14 +4,16 @@ public class baitap2 {
     public static void main(String[] args) 
     {
        Scanner scanner = new Scanner (System.in) ;
-       System.out.printf (" nhập chiều dài: ");
+       System.out.print (" nhập chiều dài: ");
        double a = scanner.nextDouble();
-       System.out.printf (" nhập chiều rộng");
+       System.out.print (" nhập chiều rộng");
        double b = scanner.nextDouble (); 
        Double chuvi = (a+b)*2;
-        System.out.printf("chu vi : "+ chuvi);
+        System.out.println("chu vi : "+ chuvi);
         Double dientich = (a*b);
-        System.out.printf("dientich : " +dientich);
+        System.out.println("dientich : " +dientich);
+        Double canhnhonhat = Math . min (a,b);
+        System.out.println("cạnh nhỏ nhất: "+ canhnhonhat);
 
     
     }
