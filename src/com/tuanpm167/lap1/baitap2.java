c3a76441d23d0d0c3c0bab97ca276ce331a8596d
tũ4an <foeveralone999@gmail.com>
2021-09-29 21:40:02 +0700

    baitap2

--- src/com/tuanpm167/lap1/baitap2.java
+++ src/com/tuanpm167/lap1/baitap2.java
@@ -1,6 +1,6 @@
 package com.tuanpm167.lap1;
 import java.util.Scanner;
-public class baitap2 {
+public class  {
     public static void main(String[] args) 
     {
        Scanner scanner = new Scanner (System.in) ;
