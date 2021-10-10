a0fb7b97dfea8c86a704d8e7a8076a204e205cab
tũ4an <foeveralone999@gmail.com>
2021-10-11 00:18:05 +0700

    baitap2

---
+++ src/com/tuanpm2008110167/lap3/baitap2.java
@@ -0,0 +1,14 @@
+package com.tuanpm2008110167.lap3;
+public class baitap2 {
+public static void main (String [] args){
+    for(int i=1; i<=9;i++)
+    {
+        System.out.println ("bảng nhân" +i);
+        for ( int j =  1 ; j <= 9 ; j ++ ){
+
+        System.out.println (i + " * " + j + " = " + i * j);
+    }
+
+} 
+}
+}
