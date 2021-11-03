45f1570a4fd83ce2c67a2b280e4bc4c379342695
tũ4an <foeveralone999@gmail.com>
2021-11-03 19:43:21 +0700

    cow

---
+++ src/com/tuanpm2008110167/lap5/cow.java
@@ -0,0 +1,17 @@
+package com.tuanpm2008110167.lap5;
+
+public class cow {
+    double weight;
+    int age;
+    cow () {
+        weight = 50.5;
+    }
+    cow (double w){
+        weight = w;
+    }
+    void moo (int moonumber ){
+        int bienCucBo = 5;
+        System.out.println (bienCucBo);
+        System.out.println ("moo...moo...");
+    }
+}
---
+++ src/com/tuanpm2008110167/lap5/cowTestDrive.java
@@ -0,0 +1,8 @@
+package com.tuanpm2008110167.lap5;
+
+public class cowTestDrive {
+ public static void main(String[] args) {
+        cow cow ;
+        cow = new cow();
+    }
+}
