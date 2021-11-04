7ea4df6928fa46a37ff63e5773d462b2b96d14e3
tũ4an <foeveralone999@gmail.com>
2021-11-04 07:20:03 +0700

    dog

---
+++ src/com/tuanpm2008110167/lap5/Dog.java
@@ -0,0 +1,10 @@
+package com.tuanpm2008110167.lap5;
+
+public class Dog {
+    int size ;
+    String breed;
+    String name;
+    void bark (){
+        System.out.println (" gâu...gâu...ẳng...ẳng");
+    }
+}
---
+++ src/com/tuanpm2008110167/lap5/DogTestDrive.java
@@ -0,0 +1,9 @@
+package com.tuanpm2008110167.lap5;
+
+public class DogTestDrive {
+    public static void name(String[] args) {
+        Dog dog = new Dog ();
+        
+    }
+    
+}
